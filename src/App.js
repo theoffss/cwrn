21967ebf1b166bae6cd314fb2b751998e334bc97
Jeremy Thomas <theoffss@hotmail.com>
2020-04-25 19:39:16 +0200

    fin video 1 lesson 6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import { Switch,  Route } from 'react-router-dom';
 import HomePage from './components/pages/homepage/homepage.component'
+import ShopPage from './components/pages/shop/shop.component'
 
 import './App.css';
-
-const HatsPage = () => (
-
-  <div>
-    <h1> Hats Page </h1>
-  </div>
-);
- 
-
-
-
 
 function App() {
   return (
     <div>
       <Switch>
        <Route exact path='/' component= {HomePage} />
-       <Route exact path='/hats' component= {HatsPage} />
+       <Route exact path='/shop' component= {ShopPage} />
        </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-//match : vidéo 5.3, il s'agit de récuper via le props l'adresse url actuelle dynamiquement
+//match : vidéo 5.3, il s'agit de récuper via le props l'adresse url actuelle dynamiquement grace à withrouter
 const MenuItem =({ title, imageUrl, size , history, linkUrl, match})=>(
     <div className= {`${size}   menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
       <div 
@@ -20,5 +20,5 @@ const MenuItem =({ title, imageUrl, size , history, linkUrl, match})=>(
 
 );
 
-
+ 
 export default withRouter(MenuItem); 
