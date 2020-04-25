0f7fb23a629843dc40168c5c361ef701566fa8de
Jeremy Thomas <jeremythomas@confederation.fo>
2020-04-25 18:19:22 +0200

    fin chapitre 5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-
+import { Switch,  Route } from 'react-router-dom';
 import HomePage from './components/pages/homepage/homepage.component'
 
 import './App.css';
 
+const HatsPage = () => (
+
+  <div>
+    <h1> Hats Page </h1>
+  </div>
+);
+ 
+
+
+
+
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      <Switch>
+       <Route exact path='/' component= {HomePage} />
+       <Route exact path='/hats' component= {HatsPage} />
+       </Switch>
     </div>
   );
 }
